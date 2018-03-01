b3b9216131b1ed988cc8ab95f940c899c9e3cbbf
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-03-01 11:39:06 -0300

    Update helper.bat

--- pendrive helper/helper.bat
+++ pendrive helper/helper.bat
@@ -3,13 +3,14 @@
 SET unit=E
 SET /P unit=Insira a unidade de disco do seu dispositivo
 
-:a                                    ::verifica e exlui todos os arquivos que NÃO ESTÃO ocultos
+:a                                    
+                                            ::verifica e exlui todos os arquivos que NÃO ESTÃO ocultos
 
 SET f=teste
 DIR /B /A -H -S| SET /P f             ::incompleto                         
 
 
-FOR /f %%A IN ('DIR /a-d-s-h /b ^| FIND /v /c ""') DO SET cnt=%%A       ::se ainda houver arquivos não ocultos, volta para o começo da rotina
+FOR /f %%A IN ('DIR /a-d-s-h /b ^| FIND /v /c ""') DO SET cnt=%%A     ::se ainda houver arquivos não ocultos, volta para o começo da rotina
 IF %A%>0 GOTO a
 
 ATTRIB %unit%:\*.* /d /s +a -h -r -s
