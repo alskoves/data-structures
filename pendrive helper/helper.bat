909e886374e4d23141ba0d2e6f5bcbfc1d9b73f2
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-03-01 11:38:24 -0300

    Update helper.bat

--- pendrive helper/helper.bat
+++ pendrive helper/helper.bat
@@ -3,7 +3,14 @@
 SET unit=E
 SET /P unit=Insira a unidade de disco do seu dispositivo
 
+:a                                    ::verifica e exlui todos os arquivos que NÃO ESTÃO ocultos
 
+SET f=teste
+DIR /B /A -H -S| SET /P f             ::incompleto                         
+
+
+FOR /f %%A IN ('DIR /a-d-s-h /b ^| FIND /v /c ""') DO SET cnt=%%A       ::se ainda houver arquivos não ocultos, volta para o começo da rotina
+IF %A%>0 GOTO a
 
 ATTRIB %unit%:\*.* /d /s +a -h -r -s
 
@@ -11,5 +18,5 @@ DEL "%unit%:\iso.vbs"
 DEL "%unit%:\SystemValueInformation"
 
 ECHO.Seus arquivos devem aparecer novamente. Obrigado por utilizar.
-
+PAUSE
 EXIT
