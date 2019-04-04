ba244c8f0646c2c4f3baa54ecb09b924ae8d4a94
alskoves <sokulski@alunos.utfpr.edu.br>
2019-04-04 13:35:16 -0300

    Update arvorebusca.h

--- arvorebusca.h
+++ arvorebusca.h
@@ -1,9 +1,57 @@
 #pragma once
 //árvores de busca binária
 
-template<class T1>
+template<class T>
 class Arvore{
 
+protected:
 
+    T data;
+    Arvore<T>* esq;
+    Arvore<T>* dir;
+    
+protected:
+    
+    mataArvore(Arvore<T>* arv){
+     
+        if(arv->esq != NULL){
+            
+            mataArvore(esq);
+            
+        }
+        if(arv->dir != NULL){
+            
+            mataArvore(dir);
+            
+        }
+        if(arv->esq == NULL && arv->esq == NULL){
+            
+            
+        
+    }
+    
+public:
+    
+    Arvore(T1 d){
+        
+        data = d;
+        esq = NULL;
+        dir = NULL;
+    
+    }
+    
+    Arvore(){
+        
+        esq = NULL;
+        dir = NULL;
+           
+    }
+    
+    ~Arvore(){
+        
+        mataArvore(this);   
+        
+    }
+    
 
 };
