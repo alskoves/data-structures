d23c20876fdadb743e1b346baa28409fcb593e99
alskoves <sokulski@alunos.utfpr.edu.br>
2019-04-11 13:50:35 -0300

    Update arvoreavl.h

--- arvoreavl.h
+++ arvoreavl.h
@@ -1,6 +1,9 @@
 #pragma once
 //código do professor minetto
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node{
 
     char data;
@@ -10,9 +13,31 @@ typedef struct node{
 
 } No, Arvore;
 
-Arvore* Inserir(Arvore* a, char chave){
+Arvore* Inserir(Arvore* a, char data){
 
-    if(a==NULL)
+    if(a==NULL){
+        
+        a = (No*) malloc(sizeof(No));
+        a->data = data;
+        a->altura = 0;
+        a->esq = NULL;
+        a->dir = NULL;
+        
+    }
+    
+    else if(data < a->data){
+        
+        a->esq = Inserir(a->esq, data);
+        a->altura = Atualizar_fb_esq(a);
+        
+    }
+    
+    else{
+        
+        a->dir = Inserir(a->dir, data);
+        a->altura = Atualizar_fb_dir(a);
+        
+    }
 
 }
 
@@ -114,4 +139,80 @@ Arvore* Rotacao_dupla_dir(Arvore* a){
     a->esq = Rotacao_simples_esq(a->esq);
     return Rotacao_simples_dir(a);
     
+}
+
+Arvore* remover (Arvore *a, char chave) {
+   if (a == NULL) {
+      return NULL;
+   }
+   else {
+      if (chave < a->chave) {
+         a->esq = remover (a->esq, chave);
+         a = atualizar_fb_dir (a);
+      }
+      else if (chave > a->chave) {
+         a->dir = remover (a->dir, chave);
+         a = atualizar_fb_esq (a);
+      }
+      else {
+         if ((a->esq == NULL) && (a->dir == NULL)) {
+            free (a);
+            a = NULL;
+         }
+         else if (a->esq == NULL) {
+            No *tmp = a;
+            a = a->dir;
+            free (tmp);
+         }
+         else if (a->dir == NULL) {
+            No *tmp = a;
+            a = a->esq;
+            free (tmp);
+         }
+         else {
+            No *tmp = a->esq;
+            while (tmp->dir != NULL) {
+               tmp = tmp->dir;
+            }
+            a->chave = tmp->chave; 
+            tmp->chave = chave;
+            a->esq = remover (a->esq, chave); 
+            a = atualizar_fb_dir (a);
+         }
+      }
+      return a; 
+   }   
+}
+
+void imprimir_in_order (Arvore* a, int nivel) {
+   if (a != NULL) {
+      int i;
+      for (i = 0; i < nivel; i++) {
+         printf("      ");
+      }
+      printf("Chave: %c (altura: %d, fb: %+d) no nível: %d\n", a->chave, a->altura, balanceamento(a), nivel);
+      imprimir_in_order (a->esq, nivel + 1);
+      imprimir_in_order (a->dir, nivel + 1);
+   }
+}
+
+int main () {
+
+   Arvore *AVL = NULL;
+
+   AVL = inserir (AVL, 'Q');
+   /*Complete!!!*/
+
+   //AVL = remover (AVL, 'A');
+   //AVL = remover (AVL, 'H');
+   //AVL = remover (AVL, 'E');
+   //AVL = remover (AVL, 'W');
+   //AVL = remover (AVL, 'G');
+   //AVL = remover (AVL, 'N');
+   //AVL = remover (AVL, 'P');
+   //AVL = remover (AVL, 'O');
+
+   imprimir_in_order (AVL, 0);
+
+   return 0;
 }
